9d51ae651a1c39a83b0192fbbe1e400abf3a0409
Christoph Hellwig <hch@lst.de>
2018-01-22 19:45:05 +0100

    bump version to v0.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name = 'nvmetcli',
-    version = 0.5,
+    version = 0.6,
     description = 'NVMe target configuration tool',
     license = 'Apache 2.0',
     maintainer = 'Christoph Hellwig',
