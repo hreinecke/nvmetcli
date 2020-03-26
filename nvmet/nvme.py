226b73a4ae317c6aa3c7b00da2adbd1ec769ce34
Tony Asleson <tasleson@redhat.com>
2020-03-26 13:07:46 -0500

    nvme.py: Explicit close is redundant
    One of the benefits of using 'with' statement for open files is the
    close is going to be called regardless of what happens.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- nvmet/nvme.py
+++ nvmet/nvme.py
@@ -302,7 +302,6 @@ class Root(CFSNode):
             f.write("\n")
             f.flush()
             os.fsync(f.fileno())
-            f.close()
 
         os.rename(savefile + ".temp", savefile)
 
