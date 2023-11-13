hreinecke/nvmetcli

commit 51bb51573ad36c5ab2655c6e31106a79eb0f800d

nvmetcli: fixup ana groupid setting for namespaces
The ANA group of a namespace is displayed, but can't be set.
So add a new attribute group 'ana' to the namespace such that
it can be set via 'set ana grpid'.

Signed-off-by: Hannes Reinecke <hare@suse.de>
Signed-off-by: Christoph Hellwig <hch@lst.de>

nvmet/nvme.py
@@ -567,7 +567,7 @@ class Namespace(CFSNode):
             if nsid < 1 or nsid > self.MAX_NSID:
                 raise CFSError("NSID must be 1 to %d" % self.MAX_NSID)
 
-        self.attr_groups = ['device']
+        self.attr_groups = ['device', 'ana']
         self._subsystem = subsystem
         self._nsid = nsid
         self._path = "%s/namespaces/%d" % (self.subsystem.path, self.nsid)

commit bef8e8ddf8aba0ec0fba115752067605c6583d80

Namespace ANA group is displayed twice
When moving the namespace ANA group into it's own attribute group
we do not need to print out an extra argument 'ana_grpid' when
displaying the namespace.

Fixes: 51bb515 ("nvmetcli: fixup ana groupid setting for namespaces")
Signed-off-by: Hannes Reinecke <hare@suse.de>

nvmet/nvme.py
@@ -626,7 +626,6 @@ class Namespace(CFSNode):
     def dump(self):
         d = super(Namespace, self).dump()
         d['nsid'] = self.nsid
-        d['ana_grpid'] = self.grpid
         return d
 
 