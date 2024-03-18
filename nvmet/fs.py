hreinecke/nvmetcli

commit b6935af23c3c290e5ec9131ee4f9fb8a7f3e7490

nvmet: Add a json-based REST API
Separate out the backend calls from nvme into a separate module 'fs.py',
and add a json-based REST API backend to allow for remote configuration.

Signed-off-by: Hannes Reinecke <hare@suse.de>

nvmet/fs.py
@@ -1,0 +1,89 @@
+'''
+Filesystem abstraction for NVMe target hierarchy
+
+Copyright (c) 2023 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+'''
+
+import stat
+import os
+from glob import iglob as glob
+
+from .error import CFSError, CFSNotFound
+
+class Filesystem(object):
+
+    configfs_dir = '/sys/kernel/config'
+
+    def create(self, path):
+        os.mkdir(f'{self.configfs_dir}/{path}')
+
+    def fetch(self, path):
+        cfs_path = f'{self.configfs_dir}{path}'
+        try:
+            with open(cfs_path, 'r') as fd:
+                return fd.read().strip()
+        except FileNotFoundError:
+            raise CFSNotFound(f'Cannot find {cfs_path}')
+        except Exception as e:
+            raise CFSError(f'Failed to read {cfs_path}: {e}')
+
+    def fetch_list(self, path, value):
+        return [os.path.basename(name)
+                for name in os.listdir(f'{self.configfs_dir}{path}/{value}/')]
+
+    def modify(self, path, value):
+        cfs_path = f'{self.configfs_dir}{path}'
+
+        if not os.path.isfile(cfs_path):
+            raise CFSNotFount(f'Cannot find attribute: {cfs_path}')
+        try:
+            with open(f'{self.configfs_dir}{path}', 'w') as fd:
+                fd.write(str(value))
+        except Exception as e:
+            raise CFSError(f'Cannot set attribute {path}: {e}')
+
+    def remove(self, path):
+        cfs_path = f'{self.configfs_dir}{path}'
+
+        if not os.path.isdir(cfs_path):
+            return
+        try:
+            os.rmdir(cfs_path)
+        except Exception as e:
+            raise CFSError(f'Failed to remove {path}: {e}')
+
+    def link(self, src, dst):
+        os.symlink(f'{self.configfs_dir}{src}', f'{self.configfs_dir}{dst}')
+
+    def unlink(self, path):
+        os.unlink(f'{self.configfs_dir}{path}')
+
+    def test(self, path):
+        return os.path.isdir(f'{self.configfs_dir}{path}')
+
+    def test_attr(self, path):
+        return os.path.isfile(f'{self.configfs_dir}{path}')
+
+    def test_writable(self, path):
+        s = os.stat(f'{self.configfs_dir}{path}')
+        return s[stat.ST_MODE] & stat.S_IWUSR
+
+    def attr_names(self, path, group):
+        return [os.path.basename(name).split('_', 1)[1]
+                for name in glob("%s%s/%s_*" % (self.configfs_dir, path, group))
+                    if os.path.isfile(name)]
+
+    def namelist(self, path, name):
+        return os.listdir(f'{self.configfs_dir}{path}/{name}/')

nvmet/__init__.py
@@ -1,2 +1,4 @@
 from .nvme import Root, Subsystem, Namespace, Port, Host, Referral, ANAGroup,\
     DEFAULT_SAVE_FILE
+from .fs import Filesystem
+from .rpc import RESTapi

nvmet/nvme.py
@@ -19,24 +19,29 @@ under the License.
 '''
 
 import os
-import stat
 import uuid
 import json
-from glob import iglob as glob
 from six import iteritems, moves
 from .error import CFSError, CFSNotFound
+from .fs import Filesystem
+from .rpc import RESTapi
 
 DEFAULT_SAVE_FILE = '/etc/nvmet/config.json'
 
+
 class CFSNode(object):
 
-    configfs_dir = '/sys/kernel/config'
     nvmet_prefix = '/nvmet'
 
     def __init__(self):
         self._path = self.nvmet_prefix
         self._enable = None
         self.attr_groups = []
+        if 'NVMET_URI' in os.environ:
+            uri = os.environ['NVMET_URI']
+            self.cfg = RESTapi(uri)
+        else:
+            self.cfg = Filesystem()
 
     def __eq__(self, other):
         return self._path == other._path
@@ -45,7 +50,7 @@ class CFSNode(object):
         return self._path != other._path
 
     def _get_path(self):
-        return f'{self.configfs_dir}/{self._path}'
+        return self._path
 
     def _create_in_cfs(self, mode):
         '''
@@ -66,14 +71,14 @@ class CFSNode(object):
 
         if not self.exists:
             try:
-                os.mkdir(self.path)
+                self.cfg.create(self._path)
             except:
                 raise CFSError("Could not create %s" %
                                self.__class__.__name__)
         self.get_enable()
 
     def _exists(self):
-        return os.path.isdir(self.path)
+        return self.cfg.test(self._path)
 
     def _check_self(self):
         if not self.exists:
@@ -91,9 +96,7 @@ class CFSNode(object):
         '''
         self._check_self()
 
-        names = [os.path.basename(name).split('_', 1)[1]
-                 for name in glob("%s/%s_*" % (self.path, group))
-                     if os.path.isfile(name)]
+        names = self.cfg.attr_names(self._path, group)
 
         if writable is True:
             names = [name for name in names
@@ -106,8 +109,7 @@ class CFSNode(object):
         return names
 
     def _attr_is_writable(self, group, name):
-        s = os.stat("%s/%s_%s" % (self.path, group, name))
-        return s[stat.ST_MODE] & stat.S_IWUSR
+        return self.cfg.test_writable(f'{self._path}/{group}_{name}')
 
     def set_attr(self, group, attribute, value):
         '''
@@ -119,20 +121,13 @@ class CFSNode(object):
         @type value: string
         '''
         self._check_self()
-        path = "%s/%s_%s" % (self.path, str(group), str(attribute))
-
-        if not os.path.isfile(path):
-            raise CFSError("Cannot find attribute: %s" % path)
+        path = "%s/%s_%s" % (self._path, str(group), str(attribute))
 
         if self._enable:
             raise CFSError("Cannot set attribute while %s is enabled" %
                            self.__class__.__name__)
 
-        try:
-            with open(path, 'w') as file_fd:
-                file_fd.write(str(value))
-        except Exception as e:
-            raise CFSError("Cannot set attribute %s: %s" % (path, e))
+        self.cfg.modify(path, value)
 
     def get_attr(self, group, attribute):
         '''
@@ -142,36 +137,27 @@ class CFSNode(object):
         @return: The named attribute's value, as a string.
         '''
         self._check_self()
-        path = "%s/%s_%s" % (self.path, str(group), str(attribute))
-        if not os.path.isfile(path):
-            raise CFSError("Cannot find attribute: %s" % path)
-
-        with open(path, 'r') as file_fd:
-            return file_fd.read().strip()
+        path = "%s/%s_%s" % (self._path, str(group), str(attribute))
+        return self.cfg.fetch(path)
 
     def get_enable(self):
         self._check_self()
-        path = "%s/enable" % self.path
-        if not os.path.isfile(path):
-            return None
+        path = "%s/enable" % self._path
 
-        with open(path, 'r') as file_fd:
-            self._enable = int(file_fd.read().strip())
+        try:
+            self._enable = int(self.cfg.fetch(path))
+        except CFSNotFound:
+            return None
         return self._enable
 
     def set_enable(self, value):
         self._check_self()
-        path = "%s/enable" % self.path
+        path = "%s/enable" % self._path
 
-        if not os.path.isfile(path) or self._enable is None:
-            raise CFSError("Cannot enable %s" % self.path)
+        if self._enable is None:
+            raise CFSError("Cannot enable %s" % self._path)
 
-        try:
-            with open(path, 'w') as file_fd:
-                file_fd.write(str(value))
-        except Exception as e:
-            raise CFSError("Cannot enable %s: %s (%s)" %
-                           (self.path, e, value))
+        self.cfg.modify(path, str(value))
         self._enable = value
 
     def delete(self):
@@ -179,8 +165,7 @@ class CFSNode(object):
         If the underlying object does not exist, this method does nothing.
         If the underlying object exists, this method attempts to delete it.
         '''
-        if self.exists:
-            os.rmdir(self.path)
+        self.cfg.remove(self._path)
 
     path = property(_get_path,
                     doc="Get the full object path.")
@@ -217,12 +202,12 @@ class Root(CFSNode):
     def __init__(self):
         super(Root, self).__init__()
 
-        if not os.path.isdir(self.configfs_dir):
+        if not self.cfg.test(self.nvmet_prefix):
             self._modprobe('nvmet')
 
-        if not os.path.isdir(self.configfs_dir):
+        if not self.cfg.test(self.nvmet_prefix):
             raise CFSError("%s does not exist.  Giving up." %
-                           self.configfs_dir)
+                           self.nvmet_prefix)
 
         self._path = self.nvmet_prefix
         self._create_in_cfs('lookup')
@@ -246,7 +231,7 @@ class Root(CFSNode):
     def _list_subsystems(self):
         self._check_self()
 
-        for d in os.listdir("%s/subsystems/" % self.path):
+        for d in self.cfg.namelist(self._path, 'subsystems'):
             yield Subsystem(d, 'lookup')
 
     subsystems = property(_list_subsystems,
@@ -255,7 +240,7 @@ class Root(CFSNode):
     def _list_ports(self):
         self._check_self()
 
-        for d in os.listdir("%s/ports/" % self.path):
+        for d in self.cfg.namelist(self._path, 'ports'):
             yield Port(d, 'lookup')
 
     ports = property(_list_ports,
@@ -264,7 +249,7 @@ class Root(CFSNode):
     def _list_hosts(self):
         self._check_self()
 
-        for h in os.listdir("%s/hosts/" % self.path):
+        for h in self.cfg.namelist(self._path, 'hosts'):
             yield Host(h, 'lookup')
 
     hosts = property(_list_hosts,
@@ -439,15 +424,14 @@ class Subsystem(CFSNode):
 
     def _list_namespaces(self):
         self._check_self()
-        for d in os.listdir("%s/namespaces/" % self.path):
+        for d in self.cfg.namelist(self._path, 'namespaces'):
             yield Namespace(self, int(d), 'lookup')
 
     namespaces = property(_list_namespaces,
                           doc="Get the list of Namespaces for the Subsystem.")
 
     def _list_allowed_hosts(self):
-        return [os.path.basename(name)
-                for name in os.listdir("%s/allowed_hosts/" % self.path)]
+        return self.cfg.fetch_list(self._path, 'allowed_hosts')
 
     allowed_hosts = property(_list_allowed_hosts,
                              doc="Get the list of Allowed Hosts for the Subsystem.")
@@ -456,18 +440,19 @@ class Subsystem(CFSNode):
         '''
         Enable access for the host identified by I{nqn} to the Subsystem
         '''
+        src = f'{self.nvmet_prefix}/hosts/{nqn}'
+        dst = f'{self._path}/allowed_hosts/{nqn}'
         try:
-            os.symlink("%s%s/hosts/%s" % (self.configfs_dir, self.nvmet_prefix, nqn),
-                       "%s/allowed_hosts/%s" % (self.path, nqn))
+            self.cfg.link(src, dst)
         except Exception as e:
-            raise CFSError("Could not enable access to host %s: %s" % (nqn, e))
+            raise CFSError(f'Could not enable access from {src} to {dst}: {e}')
 
     def remove_allowed_host(self, nqn):
         '''
         Disable access for the host identified by I{nqn} to the Subsystem
         '''
         try:
-            os.unlink("%s/allowed_hosts/%s" % (self.path, nqn))
+            self.cfg.unlink(f'{self._path}/allowed_hosts/{nqn}')
         except Exception as e:
             raise CFSError("Could not disable access to %s: %s" % (nqn, e))
 
@@ -565,18 +550,15 @@ class Namespace(CFSNode):
     def _get_grpid(self):
         self._check_self()
         _grpid = 0
-        path = "%s/ana_grpid" % self.path
-        if os.path.isfile(path):
-            with open(path, 'r') as file_fd:
-                _grpid = int(file_fd.read().strip())
+        path = "%s/ana_grpid" % self._path
+        if self.cfg.test_attr(path):
+            _grpid = int(self.cfg.fetch(path))
         return _grpid
 
     def set_grpid(self, grpid):
         self._check_self()
         path = "%s/ana_grpid" % self.path
-        if os.path.isfile(path):
-            with open(path, 'w') as file_fd:
-                file_fd.write(str(grpid))
+        self.cfg.modify(path, str(grpid))
 
     grpid = property(_get_grpid, doc="Get the ANA Group ID.")
 
@@ -636,8 +618,7 @@ class Port(CFSNode):
     portid = property(_get_portid, doc="Get the Port ID as an int.")
 
     def _list_subsystems(self):
-        return [os.path.basename(name)
-                for name in os.listdir("%s/subsystems/" % self.path)]
+        return self.cfg.fetch_list(self._path, 'subsystems')
 
     subsystems = property(_list_subsystems,
                           doc="Get the list of Subsystem for this Port.")
@@ -647,8 +628,8 @@ class Port(CFSNode):
         Enable access to the Subsystem identified by I{nqn} through this Port.
         '''
         try:
-            os.symlink("%s%s/subsystems/%s" % (self.configfs_dir, self.nvmet_prefix, nqn),
-                       "%s/subsystems/%s" % (self.path, nqn))
+            self.cfg.link(f'{self.nvmet_prefix}/subsystems/{nqn}',
+                          f'{self._path}/subsystems/{nqn}')
         except Exception as e:
             raise CFSError("Could not enable access to %s: %s" % (nqn, e))
 
@@ -657,7 +638,7 @@ class Port(CFSNode):
         Disable access to the Subsystem identified by I{nqn} through this Port.
         '''
         try:
-            os.unlink("%s/subsystems/%s" % (self.path, nqn))
+            self.cfg.unlink("%s/subsystems/%s" % (self._path, nqn))
         except Exception as e:
             raise CFSError("Could not disable access to %s: %s" % (nqn, e))
 
@@ -676,7 +657,7 @@ class Port(CFSNode):
 
     def _list_referrals(self):
         self._check_self()
-        for d in os.listdir("%s/referrals/" % self.path):
+        for d in self.cfg.namelist(self._path, 'referrals'):
             yield Referral(self, d, 'lookup')
 
     referrals = property(_list_referrals,
@@ -684,8 +665,8 @@ class Port(CFSNode):
 
     def _list_ana_groups(self):
         self._check_self()
-        if os.path.isdir("%s/ana_groups/" % self.path):
-            for d in os.listdir("%s/ana_groups/" % self.path):
+        if self.cfg.test("%s/ana_groups" % self._path):
+            for d in self.cfg.namelist(self._path, 'ana_groups'):
                 yield ANAGroup(self, int(d), 'lookup')
 
     ana_groups = property(_list_ana_groups,
@@ -790,7 +771,7 @@ class ANAGroup(CFSNode):
     def __init__(self, port, grpid, mode='any'):
         super(ANAGroup, self).__init__()
 
-        if not os.path.isdir("%s/ana_groups" % port.path):
+        if not self.cfg.test("%s/ana_groups" % port._path):
             raise CFSError("ANA not supported")
 
         if grpid is None:

nvmet/rpc.py
@@ -1,0 +1,112 @@
+'''
+REST API abstraction for NVMe target hierarchy
+
+Copyright (c) 2023 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+'''
+
+import requests
+import json
+from glob import iglob as glob
+
+from .error import CFSError, CFSNotFound
+
+class RESTapi(object):
+
+    nvmet_url = 'http://127.0.0.1:5000'
+
+    def __init__(self, uri):
+        if uri:
+            self.nvmet_url = uri
+
+    def _get_path(self, path):
+        return f'{self.nvmet_url}/{path}'
+
+    def _error(self, r):
+        print(f'error: {r.text}')
+        m = r.reason
+        if r.status_code < 400:
+            t = r.json()
+            if t and 'message' in t:
+                m = t['message']
+        if len(m):
+            return f'{r.status_code}, {m}'
+        else:
+            return f'{r.status_code}'
+
+    def create(self, path):
+        r = requests.put(self._get_path(path))
+        if r.ok:
+            return
+        msg = self._error(r)
+        raise CFSError(f'Failed to create {path}: {msg}')
+
+    def fetch(self, path):
+        r = requests.get(self._get_path(path))
+        if r.status_code == requests.codes.not_found:
+            raise CFSNotFound(f'Cannot find attribute {path}')
+        elif not r.ok:
+            msg = self._error(r)
+            raise CFSError(f'Failed to get {path}: {msg}')
+        return r.json()
+
+    def fetch_list(self, path, value):
+        return self.fetch(f'{path}/{value}')
+
+    def modify(self, path, value):
+        r = requests.put(self._get_path(path), str(value))
+        if r.status_code == requests.codes.not_found:
+            raise CFSNotFound(f'Cannot find attribute: {path}')
+        elif not r.ok:
+            msg = self._error(r)
+            raise CFSError(f'Cannot set {path} to {value}: {msg}')
+
+    def remove(self, path):
+        r = requests.delete(self._get_path(path))
+        if r.status_code == requests.codes.not_found:
+            return
+        elif r.status_code == requests.codes.no_content:
+            return
+        elif not r.ok:
+            msg = self._error(r)
+            raise CFSError(f'Cannot delete {path}: {msg}')
+
+    def link(self, src, dst):
+        self.create(dst)
+
+    def unlink(self, path):
+        self.remove(path)
+
+    def test(self, path):
+        r = requests.head(self._get_path(path))
+        if not r.ok:
+            return False
+        else:
+            return True
+
+    def test_attr(self, path):
+        return self.test(path)
+
+    def test_writable(self, path):
+        # No mapping for REST API
+        return True
+
+    def attr_names(self, path, group):
+        names = self.fetch(f'{path}')
+        if group in names:
+            return list(names[group])
+        return {}
+
+    def namelist(self, path, name):
+        return self.fetch(f'{path}/{name}')

nvmetcli
@@ -714,7 +714,7 @@ funcs = dict(save=save, restore=restore, clear=clear, ls=ls)
 
 
 def main():
-    if os.geteuid() != 0:
+    if 'NVMET_URI' not in os.environ and os.geteuid() != 0:
         print("%s: must run as root." % sys.argv[0], file=sys.stderr)
         sys.exit(-1)
 

commit 97a5f39c59623f6ffc12c5a1dfd5d4514d1deceb

nvmet/fs: fix typo in the exception class name
Fixes: b6935af23c3c ("nvmet: Add a json-based REST API")
Signed-off-by: Daniel Wagner <dwagner@suse.de>

nvmet/fs.py
@@ -47,7 +47,7 @@ class Filesystem(object):
         cfs_path = f'{self.configfs_dir}{path}'
 
         if not os.path.isfile(cfs_path):
-            raise CFSNotFount(f'Cannot find attribute: {cfs_path}')
+            raise CFSNotFound(f'Cannot find attribute: {cfs_path}')
         try:
             with open(f'{self.configfs_dir}{path}', 'w') as fd:
                 fd.write(str(value))